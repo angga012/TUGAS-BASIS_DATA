6fe15c537b2a900ed2c3991f0c70691f9b20b693
rifkyseptian <rifkyseptiank12@gmail.com>
2026-06-23 19:40:17 +0700

    Delete backend directory

--- backend/routes/booking.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { addBooking, getBookings } from "../controllers/booking.controller";
-
-const router = Router();
-
-router.post("/", addBooking);
-router.get("/", getBookings);
-
-export default router;
